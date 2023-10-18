16eced45ce625fb76a4beb60815eb04daae4432d
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2023-10-18 18:33:25 +0300

    Server <> Refactoring index.js to fit ES6 Standards

---
+++ server/config.js
@@ -0,0 +1,5 @@
+const PORT = 3001
+
+const mongoDBURL = "mongodb+srv://ahmad9:Monkey@cluster0.3xxbfxh.mongodb.net/?retryWrites=true&w=majority"
+
+export { PORT, mongoDBURL }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
-const express = require('express')
-const  mongoose = require('mongoose')
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bcrypt from 'bcrypt';
+import body from 'body-parser';
+import jwt from 'jsonwebtoken';
+import { PORT, mongoDBURL } from "./config.js";
 
-const cors = require('cors')
-const bodyParser = require('body-parser')
-
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
 
 
 const app = express()
-const PORT = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log("App connected to database");
+    
+    app.listen(PORT, () => {
+        console.log(`App listening to port: ${PORT}`);
+    });
+
+    
+    
+}).catch((err) => {
+    console.log(err);
+})
